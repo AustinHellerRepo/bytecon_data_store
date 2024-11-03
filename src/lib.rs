653720228eb42d1a4f2f914b2ed2066614724de8
austin <galaky2005@gmail.com>
2024-11-03 17:57:16 -0500

    wip; need to finish list trait function;

--- src/lib.rs
+++ src/lib.rs
@@ -10,4 +10,5 @@ pub trait DataStore {
     fn insert(&mut self, item: Self::Item) -> impl Future<Output = Result<Self::Key, Box<dyn Error>>> + Send;
     fn get(&self, id: &Self::Key) -> impl Future<Output = Result<Self::Item, Box<dyn Error>>> + Send;
     fn delete(&self, id: &Self::Key) -> impl Future<Output = Result<(), Box<dyn Error>>> + Send;
+    fn list(&self, page_index: u64, page_size: u64, row_offset: u64) -> impl Future<Output = Result<Vec<Self::Key>, Box<dyn Error>>> + Send;
 }
